7e6c9cd753eb8e3af868b2879616958449ff40b6
mattdchilds <118834601+mattdchilds@users.noreply.github.com>
2023-03-28 20:02:57 -0500

    Staged so future formats are easier

--- scripts/formatting.py
+++ scripts/formatting.py
@@ -2,7 +2,15 @@ import re
 
 #NCBS text formatting
 
-def format_text_from_ncbs(text, book_name):
+def format_text(text, book_name):
+    if book_name == "Swedenborg Dot Com":
+        print("Swedenborg.com file detected")
+        references, content = format_text_from_swedenborg_dot_com(text, book_name)
+    else:
+        references, content = format_text_from_ncbs(text, book_name)
+    return references, content
+
+def format_text_from_ncbs(text, book_name): #Sort of works. Not great. I'm planning to rewrite.
 
     markup_new_section = r'ppp\d+#pid#\d+.'
     markup_new_section_short = "#pid#"
@@ -79,4 +87,55 @@ def format_text_from_ncbs(text, book_name):
 
     return references, content
 
-     
+def format_text_from_swedenborg_dot_com(text, book_name):
+    
+    markup_new_section = r"###(.*?)###"
+    markup_new_subsection = r"@@@(.*?)@@@"
+
+    def clean(): #Removes formatting
+        content[-1] = re.sub(markup_new_section,"",content[-1])
+        content[-1] = re.sub(markup_new_subsection,"",content[-1])
+
+
+    references = [""]
+    content = [""]
+
+    reference_markup = ""
+    subsection_number = "" #Defaults to [1] when a new section starts
+
+    text_file_lines = re.split("\n\n", text)
+
+
+    #Content formatting, (reference gets formatted a little)
+
+    for i, line in enumerate(text_file_lines):
+
+        #Checks for new section
+
+        if re.search(markup_new_section, line):
+            subsection_number = ""
+
+            reference_markup = re.search(markup_new_section, line).group()
+            references.append(reference_markup[:-1])
+
+            content.append(line)
+        
+        #Checks for new subsection
+
+        elif re.search(markup_new_subsection, line):
+            subsection_number = re.search(markup_new_subsection, line).group()
+
+            references.append(reference_markup[:-1] + subsection_number)
+
+            content.append(line)
+        
+        else:
+            content[-1] += "\n\n" + line
+        
+        clean()
+    for i, item in enumerate(references):
+
+        references[i] = book_name + " " + references[i]
+
+
+    return references, content
